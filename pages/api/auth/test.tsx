c564849eb835d65ea8aafbb6f8661425c3f94ab4
Samir Diaz <samir.diaz.1206@gmail.com>
2022-05-02 17:50:45 -0500

    se agrego cors a endpoin test

--- pages/api/auth/test.tsx
+++ pages/api/auth/test.tsx
@@ -1,5 +1,12 @@
-const handler = (req, res) => {
-	res.status(200).json({ message: 'test ok' });
+import NextCors from 'nextjs-cors';
+
+const handler = async (req, res) => {
+	await NextCors(req, res, {
+		methods: ['GET'],
+		origin: '*'
+ });
+
+ res.json({ message: 'Hello NextJs Cors!' });
 }
 
 export default handler;
